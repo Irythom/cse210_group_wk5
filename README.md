10c8578f2cdd855e8d4b537de71144d61e757e61
Irythom <ryanweinheimer@gmail.com>
2022-02-09 17:48:30 -0700

    Encapsulation in director and README contributions

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # Jumper
+
 Play one of the most entertaining puzzles! The jumper is a guessing game that starts with a hidden word and a full parachute and jumper figure. You need to guess one letter of this word at a time. If the letter is not in the word, the parachute loses part of the parachute. Whenever you find a letter in the word, then the parachute remains the same. Here is the goal: you need to guess the word before you lose the jumper's head ^-^. Good luck!
 
 ## Getting Started
+
 ---
-Make sure you have Python 3.8.0 or newer installed and running on your machine. Open a terminal and 
+
+Make sure you have Python 3.8.0 or newer installed and running on your machine. Open a terminal and
 browse to the project's root folder. Start the program by running the following command.
+
 ```
 python3 jumper
 ```
-You can also run the program from an IDE like Visual Studio Code. Start your IDE and open the 
+
+You can also run the program from an IDE like Visual Studio Code. Start your IDE and open the
 project folder. Select the main module inside the dice folder and click the "run" button.
 
 ## Project Structure
+
 ---
+
 The project files and folders are organized as follows:
+
 ```
 root                    (project root folder)
 +-- Jumper              (project team folder)
@@ -23,13 +31,18 @@ root                    (project root folder)
 ```
 
 ## Required Technologies
+
 ---
-* Python 3.8.0
+
+- Python 3.8.0
 
 ## Authors
+
 ---
-* Miguel López (lop18028@byui.edu)
-* Ryan Weinheimer (ryanweinheimer@gmail.com)
-* Diana Guerra (diana.1609@hotmail.com)
-* Eduardo Mosquera Galarza (mos21008@byui.edu)
-* Chioneso Chatayika(cchatayika@outlook.com)
+
+- 2 PM team: Combined effort to code player.py and terminal service.py. Divided director.py by one function per individual, and volunteered for finishing touches/ editing.
+- Miguel López (lop18028@byui.edu) : Coded the updates portion of the director class, main.py, parachute.py, and oversaw the review of the project as a whole. Submitted the assignment as the group lead for the week.
+- Ryan Weinheimer (ryanweinheimer@gmail.com) : coded the init portion of the director class, and added encapsulation to the entire director class. Added contributions to README
+- Diana Guerra (diana.1609@hotmail.com) : Coded the start game portion of the director class. Added words to the generate word portion of player class. Put together README file.
+- Eduardo Mosquera Galarza (mos21008@byui.edu) : Coded the outputs portion of the director class. Reviewed and edited project.
+- Chioneso Chatayika(cchatayika@outlook.com) : Coded the inputs portion of the director class. Reviewed and edited project.
--- jumper/director.py
+++ jumper/director.py
@@ -2,9 +2,10 @@ from terminal_service import Terminal_service
 from player import Player
 from parachute import Parachute
 
-class Director:   
+
+class Director:
     """The responsibility of the director is to control how the game is played.
-    
+
     Stereotype:
         Controller
     Attributes:
@@ -17,68 +18,69 @@ class Director:
         jumper ( Jumper ): An instance of the class of objects known as Jumper.
         guesser ( Guesser ): An instance of the class of objects known as Guesser.
     """
-    def __init__( self ):
+
+    def __init__(self):
         """The class constructor.
-        
+
         Args:
             self (Director): an instance of Director.
         """
-        self.console = Terminal_service()
-        self.jumper = Parachute()
-        self.guesser = Player()
-        self.keep_playing = True
-        self.word_guessed = False
-        self.parachute = self.jumper.return_parachute()
-        self.current_guess = self.guesser.return_guess()
-        self.letter = ""
-        
-    def start_game( self ):
+        self._console = Terminal_service()
+        self._jumper = Parachute()
+        self._guesser = Player()
+        self._keep_playing = True
+        self._word_guessed = False
+        self._parachute = self._jumper.return_parachute()
+        self._current_guess = self._guesser.return_guess()
+        self._letter = ""
+
+    def start_game(self):
         """Starts the game loop to control how the game is played.
-        
+
         Args:
             self ( Director ): an instance of Director.
         """
-        self.guesser.generate_word()
-        self.guesser.generate_lines()
+        self._guesser.generate_word()
+        self._guesser.generate_lines()
         self.do_outputs()
-        while self.keep_playing:
+        while self._keep_playing:
             self.get_inputs()
             self.do_updates()
             self.do_outputs()
 
-    def get_inputs( self ):
+    def get_inputs(self):
         """Gets the inputs at the beginning of each round of play. In this instance it means
         getting a letter from the user.
         Args:
             self ( Director ): An instance of Director.
         """
-        self.letter = self.console.get_letter()
+        self._letter = self._console.get_letter()
 
-    def do_updates( self ):
+    def do_updates(self):
         """Updates the important game information for each round of play. In this case
         that means checking the parachute and cutting off part if user guess is wrong.
         As well as keeping track of correct guesses and updating the current word.
         Args:
             self ( Director ): An instance of Director.
         """
-        good_parachute = self.jumper.check_parachute()
-        if good_parachute == True and self.word_guessed == False:
-            letter = self.letter
-            valid_guess = self.guesser.check_guess( letter )
-            self.current_guess = self.guesser.return_guess()
-            self.jumper.cut_parachute( valid_guess )
-            self.parachute = self.jumper.return_parachute()
-            self.word_guessed = self.guesser.check_word()
+        good_parachute = self._jumper.check_parachute()
+        if good_parachute == True and self._word_guessed == False:
+            letter = self._letter
+            valid_guess = self._guesser.check_guess(letter)
+            self._current_guess = self._guesser.return_guess()
+            self._jumper.cut_parachute(valid_guess)
+            self._parachute = self._jumper.return_parachute()
+            self._word_guessed = self._guesser.check_word()
         elif good_parachute == False:
-            self.keep_playing = False
-        if self.word_guessed == True:
-            self.keep_playing = False
-        
-    def do_outputs( self ):
+            self._keep_playing = False
+        if self._word_guessed == True:
+            self._keep_playing = False
+
+    def do_outputs(self):
         """Outputs the important game information for each round of play. Such as the current word
         and the status of the jumper.
         Args:
             self ( Director ): An instance of Director.
         """
-        self.console.display_guess( self.current_guess )
-        self.console.display_parachute( self.jumper.parachute )
+        self._console.display_guess(self._current_guess)
+        self._console.display_parachute(self._jumper.parachute)
